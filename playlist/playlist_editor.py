8d8d650be5467988ea49eebbf10170d11f1a77c8
TomoBossi <tomasbossi97@gmail.com>
2023-05-25 21:47:18 -0300

    Add new music to playlist

--- playlist/playlist_editor.py
+++ playlist/playlist_editor.py
@@ -43,6 +43,12 @@ def remove(df, index):
     df = df.reset_index(drop=True)
     return df
 
+# Regiones trabajables: OSTs jazzeros entre Hotline Miami, lounge previo al DnB muy desorganizado.
+# Yuyos incomodos: Hana Valley, The Fall y Mental Diving cerca del lounge, Who can it be now.
+
+# TODO debug para, desde la consola, recorrer todas las canciones
+# chequeando existencia (thumbnail hack) y/o reproducibilidad (eventual status == "PLAYING")
+
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__))
     df = pd.read_json("playlist.json", orient = "index")
@@ -50,21 +56,21 @@ if __name__ == "__main__":
 
     if mode == "add":
         df = add(df, 
-            index = ,
-            title = "", 
-            artists = "", 
-            album = "",
-            yt_id = "",
-            yt_title = "",
-            yt_duration_s = 0,
+            index = 547,
+            title = "Better (The Whispers - I Can Make It Better; James Brown - There It Is)",
+            artists = "SAINT PEPSI",
+            album = "Hit Vibes",
+            yt_id = "WYvji5AXOfk",
+            yt_title = "SAINT PEPSI - Better",
+            yt_duration_s = 234,
             yt_start_s = 0,
             yt_end_s = 0,
             volume_multiplier = 1.0,
-            album_cover_filename = ".jpg",
+            album_cover_filename = "saint_pepsi_cherry_pepsi_better_alt1.jpg",
         )
 
     if mode == "move":
-        df = move(df, a, a)
+        df = move(df, 515, 541)
 
     if mode == "remove":
         df = remove(df, a)
