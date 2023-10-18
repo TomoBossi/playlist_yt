5d32626cab1b8303ffea50146b4b604e12e316ca
TomoBossi <tomasbossi97@gmail.com>
2023-10-18 01:01:44 -0300

    add music

--- playlist/playlist_editor.py
+++ playlist/playlist_editor.py
@@ -65,21 +65,21 @@ if __name__ == "__main__":
     import os
     os.chdir(os.path.dirname(__file__))
     df = pd.read_json("playlist.json", orient = "index")
-    mode = ["add", "move", "remove", "disable"][4] # safety [4]th
+    mode = ["add", "move", "remove", "disable"][0] # safety [4]th
 
     if mode == "add":
         df = add(df,
-            index = 511,
-            title = "白ヶ丘 - マロミのテーマ",
-            artists = "Susumu Hirasawa",
-            album = "Paranoia Agent OST",
-            yt_id = "pIkHBwRXBpU",
-            yt_title = "Paranoia Agent Original Soundtrack",
-            yt_duration_s = 3163,
-            yt_start_s = 2775,
-            yt_end_s = 2866,
+            index = 475,
+            title = "RAVE SHIT",
+            artists = "2 Mello",
+            album = "Sounds of Tokyo-To Future",
+            yt_id = "AlHtotv0O5g",
+            yt_title = "2 Mello - RAVE SHIT (Official Audio)",
+            yt_duration_s = 263,
+            yt_start_s = 0,
+            yt_end_s = 0,
             volume_multiplier = 1.0,
-            album_cover_filename = "paranoia_agent_ost.jpg",
+            album_cover_filename = "tokyo_future_2_mello.jpg",
         )
 
     if mode == "move":
