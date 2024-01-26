51b2a73cbe6b85ae6b18a2090966051e71cb402c
TomoBossi <tomasbossi97@gmail.com>
2024-01-26 00:06:24 -0300

    remove mid

--- playlist/playlist_editor.py
+++ playlist/playlist_editor.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     import os
     os.chdir(os.path.dirname(__file__))
     df = pd.read_json("playlist.json", orient = "index")
-    mode = ["add", "move", "remove", "disable"][0] # safety [4]th
+    mode = ["add", "move", "remove", "disable"][4] # safety [4]th
 
     if mode == "add":
         df = add(df,
