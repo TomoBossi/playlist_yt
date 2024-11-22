342ffb46969d840c3e95dc67cecfee7e9642fc7a
TomoBossi <tomasbossi97@gmail.com>
2024-11-22 01:49:47 -0300

    Drink your big black cow :)

--- playlist/playlist_editor.py
+++ playlist/playlist_editor.py
@@ -70,16 +70,16 @@ if __name__ == "__main__":
     if mode == "add":
         df = add(df,
             index = _,
-            title = "It's Going Down Now",
-            artists = "\u30a2\u30c8\u30e9\u30b9\u30b5\u30a6\u30f3\u30c9\u30c1\u30fc\u30e0",
-            album = "PERSONA3 Reload OST",
-            yt_id = "2KuWjZD6PBA",
-            yt_title = "It's Going Down Now",
-            yt_duration_s = 186,
+            title = "Black Cow",
+            artists = "Steely Dan",
+            album = "Aja",
+            yt_id = "XRj0Ohg-y1A",
+            yt_title = "Black Cow",
+            yt_duration_s = 310,
             yt_start_s = 0.0,
             yt_end_s = 0.0,
             volume_multiplier = 1.0,
-            album_cover_filename = "p3reload.jpg"
+            album_cover_filename = "5Zxv8bCtxjz11jjypNdkEa_Aja.jpg"
         )
 
     if mode == "move":
